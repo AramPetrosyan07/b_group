e1fc49ed78afbf2d0f2e9179dffc49ef70fb06d9
unknown <aarpineh@gmail.com>
2023-04-29 21:17:07 +0400

    added images and pakages

---
+++ src/Portfolio/Banner/Banner.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import classes from 'style.module.css'
+
+function Banner() {
+  return (
+    <div className={classes.banner}>
+      <img src="./" alt="" />
+    </div>
+  )
+}
+
+export default Banner
---
+++ src/Portfolio/Portfolio.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import classes from 'style.module.css'
+
+function Portfolio() {
+  return (
+    <div className={classes.baner}>
+      
+    </div>
+  )
+}
+
+export default Portfolio
