701b2bc82a8486df94837df3408325978e68621a
psuzanna <levonliamik@mail.ru>
2023-04-30 15:47:15 +0400

    30.04.2023/2

--- src/App.js
+++ src/App.js
@@ -2,18 +2,14 @@ import React from "react";
 import { data } from "./database.js";
 import "./styles/reset.css";
 import "./styles/main.css";
-import { BigBanner } from "./components/BigBanner/BigBanner";
-import { PageCaption } from "./components/PageCaption/PageCaption.js";
-import { ExpertsList } from "./components/ExpertsList/ExpertsList.js";
+import { Team } from "./pages/Team.js";
 
 
 const { team } = data.pages;
 function App() {
   return (
     <div className="App">
-      <BigBanner caption={team.bannerCaption} />
-      <PageCaption subtitle={team.subtitle} title={team.title} text={team.text} />
-      <ExpertsList experts={team.experts} />
+      <Team team={team} />
     </div>
   );
 }
