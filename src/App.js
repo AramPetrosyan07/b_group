3902360272cf6fb1951546ad8bc0122b5cc519aa
unknown <aarpineh@gmail.com>
2023-04-30 21:35:00 +0400

    changed files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
 import './App.css';
+import lemon from './images/lemon.png';
+import carrot from './images/carrot.png';
+import leaf from './images/leaf.png';
+import tommato from './images/tommato.png';
+import raspberry from './images/raspberry.png';
+import orange from './images/orange.png';
+import Banner from './Banner/Banner';
+import Portfolio from './Portfolio/Portfolio';
+import { useEffect, useState } from 'react';
+
+const images = [lemon, carrot, leaf, tommato, raspberry, orange];
 
 function App() {
+  const [image, setImage] = useState()
+
+  useEffect(()=>{
+    setImage(images)
+    
+  },[])
+  
+
   return (
     <div className="App">
+      <Banner />
+      {
+        image && image.map((img)=>{
+          return <Portfolio image={img} />
+        })
+      }
+        
       
     </div>
   );
--- src/Portfolio/Portfolio.jsx
+++ src/Portfolio/Portfolio.jsx
@@ -1,10 +1,73 @@
 import React from 'react'
-import classes from 'style.module.css'
+import classes from './style.module.css'
 
-function Portfolio() {
+function Portfolio(image) {
+console.log(image);
+
   return (
-    <div className={classes.baner}>
-      
+    <div className={classes.container}>
+      {
+        image.image.includes('lemon')? 
+        <div className={classes.imgBlock}>
+          <img src={image.image} alt="lemon.png" />
+          <div>
+            <h2>green & tasty lemon</h2>
+            <p>fruits</p>
+          </div>
+    </div>:null
+      }
+      {
+        image.image.includes('carrot')? 
+        <div className={classes.imgBlock}>
+          <img src={image.image} alt="carrot.png" />
+          <div>
+            <h2>organic carrot</h2>
+            <p>fruits</p>
+          </div>
+        </div>:null
+      }
+      {
+        image.image.includes('leaf')? 
+        <div className={classes.imgBlock}>
+          <img src={image.image} alt="leaf.png" />
+          <div>
+            <h2>organic betel leaf</h2>
+            <p>fruits</p>
+          </div>
+        </div>:null
+      }
+      {
+        image.image.includes('tommato')? 
+        <div className={classes.imgBlock}>
+          <img src={image.image} alt="tommato.png" />
+          <div>
+            <h2>natural tommato</h2>
+            <p>fruits</p>
+          </div>
+        </div>:null
+      }
+      {
+        image.image.includes('raspberry')? 
+        <div className={classes.imgBlock}>
+          <img src={image.image} alt="raspberry.png" />
+          <div>
+            <h2>blak raspberry</h2>
+            <p>fruits</p>
+          </div>
+        </div>:null
+      }
+      {
+        image.image.includes('orange')? 
+        <div className={classes.imgBlock}>
+          <img src={image.image} alt="orange.png" />
+          <div>
+            <h2>honey orange</h2>
+            <p>fruits</p>
+          </div>
+        </div>:null
+      }
+
+          
     </div>
   )
 }
