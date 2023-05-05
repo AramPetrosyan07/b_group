5588d46fc3960396b2c7963cd076a92af268eba0
Dose <mmdoseh@gmail.com>
2023-05-05 13:17:07 +0400

    Add icons for Home page

--- src/components/HomeBottom.js
+++ src/components/HomeBottom.js
@@ -1,8 +1,77 @@
+import image from './images/WhoWeAre.svg';
+import Arrow from './images/Arrow.svg';
+import Blog1 from './images/BlogPhoto1.svg';
+import Blog2 from './images/BlogPhoto2.svg';
+import admin from './images/AdminVect.svg';
+import styles from './HomeBottom.module.css';
+
 export default function HomeBottom() {
+
     return (<>
+        <body>
+            <div className={styles.WhoWeAre}>
+                <div className={styles.Content}>
+                    <p>Eco Friendly</p>
+                    <span>Econis is a Friendly <br /> Organic Store</span>
+                    <ul>
+                        <li><h5>Start with Our Company First</h5>
+                            <h2>Sed ut perspiciatis unde omnis iste natus error sit voluptat accusantium doloremque laudantium. Sed ut perspiciatis.</h2>
+                        </li>
+                        <li><h5>Learn How to Grow Yourself</h5>
+                            <h2>Sed ut perspiciatis unde omnis iste natus error sit voluptat accusantium doloremque laudantium. Sed ut perspiciatis.</h2>
+                        </li>
+                        <li><h5>Farming Strategies of Today</h5>
+                            <h2>Sed ut perspiciatis unde omnis iste natus error sit voluptat accusantium doloremque laudantium. Sed ut perspiciatis.</h2></li>
+                    </ul>
+                </div>
+                <div className={styles.Image}><img src={image} /></div>
+            </div>
 
+            <div className={styles.BottomGallery}>
+                <ul>
+                    <li><button>Organic Juice</button></li>
+                    <li><button>Organic Food</button></li>
+                    <li><button>Nuts Cookis</button></li>
+                </ul>
+            </div>
 
+            <div className={styles.BottomBlog}>
 
+                <div className={styles.BlogContent}>
+                    <p>News</p>
+                    <span>Discover weekly content about <br /> organic food, & more</span>
+                    <div className={styles.BlogBtn}>
+                        <span>More News</span>
+                        <img src={Arrow} />
+                    </div>
+                </div>
+
+                <div className={styles.BlogBlog}>
+                    <div className={styles.BlogFirst}>
+                        <span>25 <br /> now</span>
+                        <img src={Blog1} />
+                        <div className={styles.BlogFirstText}>
+                            <img src={admin} /> <span>By Rachi Card</span>
+                            <h4>The Benefits of Vitamin D & How to Get It</h4>
+                            <h2>Simply dummy text of the printing and typesetting industry. Lorem Ipsum</h2>
+                            <button></button>
+                        </div>
+                    </div>
+
+                    <div className={styles.BlogSecond}>
+                        <span>25 <br /> now</span>
+                        <img src={Blog2} />
+                        <div className={styles.BlogSecondText}>
+                            <img src={admin} /> <span>By Rachi Card</span>
+                            <h4>Our Favourite Summertime Tommeto</h4>
+                            <h2>Simply dummy text of the printing and typesetting industry. Lorem Ipsum</h2>
+                            <button></button>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </body>
     </>
     )
 }
--- src/components/HomeHeader.js
+++ src/components/HomeHeader.js
@@ -2,48 +2,52 @@ import Logo from './images/Logo.svg';
 import Search from './images/Search.svg'
 import Cart from './images/Cart Icon.svg'
 import Baner from './images/Banner.jpg'
-import Headervector from './images/HeaderVector.svg'
+import Headervector from './images/HeaderVector.svg';
+import Styles from './HomeHeader.module.css';
 export default function HomeHeader() {
+
     return (<>
-        <div className='Header'>
-            <img className='logo' src={Logo} />
-            <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li className='MenuHover'><img src={Headervector}/>Pages</li>
-                <li>Shop</li>
-                <li>Projects</li>
-                <li>News</li>
-            </ul>
-            <input type='text'></input>
-            <img className='Search' src={Search} />
-            <div className='Cart'>
-                <img src={Cart} />
-                <span>Cart(0)</span>
-            </div>
-        </div>
-
-        <div className='Banner'>
-            <img className='BanerBackground' src={Baner} />
-            <div className='BannerText'>
-                <p>100% Natural Food</p>
-                <span>Choose the best healthier way <br /> of life</span>
-                <div className='BanerBtn'></div>
-
-            </div>
-        </div>
-
-        <div className='OferrBaner'>
-            <div className='leftBaner'>
-                <p>Natural!!</p>
-                <span>Get Garden <br/> Fresh Fruits</span>
+        <body>
+            <div className={Styles.Header}>
+                <img className={Styles.logo} src={Logo} />
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li className={Styles.MenuHover}><img src={Headervector} />Pages</li>
+                    <li>Shop</li>
+                    <li>Projects</li>
+                    <li>News</li>
+                </ul>
+                <input type={Styles.text}></input>
+                <img className={Styles.Search} src={Search} />
+                <div className={Styles.Cart}>
+                    <img src={Cart} />
+                    <span>Cart(0)</span>
+                </div>
             </div>
 
-            <div className='rightBaner'>
-                <p>Offer!!</p>
-                <span>Get 10% off <br/> on Vegetables</span>
+            <div className={Styles.Banner}>
+                <img className={Styles.BanerBackground} src={Baner} />
+                <div className={Styles.BannerText}>
+                    <p>100% Natural Food</p>
+                    <span>Choose the best healthier way <br /> of life</span>
+                    <div className={Styles.BanerBtn}></div>
+
+                </div>
             </div>
-        </div>
+
+            <div className={Styles.OferrBaner}>
+                <div className={Styles.leftBaner}>
+                    <p>Natural!!</p>
+                    <span>Get Garden <br /> Fresh Fruits</span>
+                </div>
+
+                <div className={Styles.rightBaner}>
+                    <p>Offer!!</p>
+                    <span>Get 10% off <br /> on Vegetables</span>
+                </div>
+            </div>
+        </body>
     </>
     )
 }
