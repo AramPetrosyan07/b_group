4d74214dcd0dd6734895a09ca663d72503554dac
Veronika Maghakyan <108238374+Veronika1986@users.noreply.github.com>
2023-05-05 12:25:03 +0400

    finish

--- src/shop single/second/Newsite2.jsx
+++ src/shop single/second/Newsite2.jsx
@@ -31,7 +31,7 @@ export default function Newsite2() {
         <button className={style.additional}>Additional Info</button>
       </div>
         <div className={style.descr}>
-          <p>Welcome to the world of natural and organic. Here you can discover the bounty of nature. We have grown on the principles of health, ecology, and care. We aim to give our customers a healthy chemical-free meal for perfect nutrition. It offers about 8–10% carbs. Simple sugars — such as glucose and fructose — make up 70% and 80% of the carbs in raw.</p>
+          <p className={style.last}>Welcome to the world of natural and organic. Here you can discover the bounty of nature. We have grown on the principles of health, ecology, and care. We aim to give our customers a healthy chemical-free meal for perfect nutrition. It offers about 8–10% carbs. Simple sugars — such as glucose and fructose — make up 70% and 80% of the carbs in raw.</p>
         </div>
     </div>
 
