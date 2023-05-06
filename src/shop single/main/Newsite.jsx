47be61ed4f1cb1aad4abcb881632dfdba7a83c44
AramPetrosyan07 <aspetrosyan@mail.ru>
2023-05-06 15:01:23 +0400

    banner

--- src/shop single/main/Newsite.jsx
+++ src/shop single/main/Newsite.jsx
@@ -1,17 +1,16 @@
-import React from 'react'
-import style from './new.module.css'
-
+import React from "react";
+import style from "./new.module.css";
+import Banner from "../../images/Banner Image.png";
 
 export default function Newsite() {
-
   return (
-      <>
-        <section className={style.first}>
-          <div className={style.container}> 
-            <h2 className={style.title}> Shop Single </h2>
-          </div>
-        </section>
-        </>
-      )
-
+    <>
+      <section className={style.first}>
+        {/* <div className={style.container}>  */}
+        {/* </div> */}
+        <img className={style.banner} src={Banner} alt="" />
+        <h2 className={style.title}> Shop Single </h2>
+      </section>
+    </>
+  );
 }
