d1f47a308c6bbd113f5f07246e78cb1cb13c77ec
AramPetrosyan07 <aspetrosyan@mail.ru>
2023-05-08 21:53:33 +0400

    structure fix

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
-import Main from "./about/components/Main";
+import About from "./about/components/Main";
 
 function App() {
   return (
     <div className="App">
-      <Main />
+      <About />
     </div>
   );
 }
