c5cc4d98488925018aacfb3cbf065b9573ada76c
AramPetrosyan07 <aspetrosyan@mail.ru>
2023-05-09 17:02:42 +0400

    bug fix

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
+
 import ShopSingle from "./shop single/Main";
 import Shop from "./shop/main";
-import Arpine from './Arpine/Main';
-
+import Arpine from "./Arpine/Main";
 
 function App() {
-    
   return (
-    <div className="App">
-      <ShopSingle />
-      <Shop />
-      <Arpine />
-      
-    </div>
+    <>
+      <Routes>
+        <Route index element={<ShopSingle />} />
+        <Route path="/Shop" element={<Shop />} />
+        <Route path="/Portfolio" element={<Arpine />} />
+      </Routes>
+    </>
   );
 }
 
