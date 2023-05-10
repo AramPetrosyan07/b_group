a7f49cec5e754f0c3fb1fc8feeb9e382e30ea3b2
psuzanna <levonliamik@mail.ru>
2023-05-10 21:52:26 +0400

    10.05.2023

--- src/Syuzanna/BigBanner/BigBanner.js
+++ src/Syuzanna/BigBanner/BigBanner.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from "react"
 import bigBanner from "../img/team-banner.jpg"
 import styles from "./BigBanner.module.css"
@@ -11,4 +12,19 @@ export const BigBanner = ({ caption }) => {
          </h2>
       </div>
    )
+=======
+import React from "react"
+import bigBanner from "../img/team-banner.jpg"
+import styles from "./BigBanner.module.css"
+
+export const BigBanner = ({ caption }) => {
+   return (
+      <div className={styles.bigBanner}>
+         <img src={bigBanner} className={styles.bigBanner__img + ' img-fluid'} alt="" />
+         <h2 className={styles.caption} >
+            {caption}
+         </h2>
+      </div>
+   )
+>>>>>>> 8ae874e41347bb5a1784c6eb4bca06142ebfa810
 }
--- src/Syuzanna/pages/Team.js
+++ src/Syuzanna/pages/Team.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from "react";
 import { BigBanner } from "../BigBanner/BigBanner";
 import { PageCaption } from "../PageCaption/PageCaption.js";
@@ -14,3 +15,21 @@ export const Team = ({ team }) => {
 }
 
 
+=======
+import React from "react";
+import { BigBanner } from "../BigBanner/BigBanner";
+import { PageCaption } from "../PageCaption/PageCaption.js";
+import { ExpertsList } from "../ExpertsList/ExpertsList.js";
+
+export const Team = ({ team }) => {
+   return (
+      <div>
+         <BigBanner caption={team.bannerCaption} />
+         <PageCaption subtitle={team.subtitle} title={team.title} text={team.text} />
+         <ExpertsList experts={team.experts} />
+      </div>
+   );
+}
+
+
+>>>>>>> 8ae874e41347bb5a1784c6eb4bca06142ebfa810
